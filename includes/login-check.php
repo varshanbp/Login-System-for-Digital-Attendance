026f681b28dc46f3089b2dde8155cdfdee4e1c07
Varshan BP <varshanbp@gmail.com>
2016-01-04 22:37:59 +0530

    Day 5 (Update 2) - Login System Test
    Day 5 Update 2 - Login System Testing. Registration Working, Database
    Security tested and Data Security tested. Committed by Varshan BP

--- includes/login-check.php
+++ includes/login-check.php
@@ -1,33 +1,27 @@
 <?php
-require_once('db-data.php');
-$dbhost=DBHOST;
-$dbuname=DBUNAME;
-$dbpass=DBPASS;
-$dbname=DBNAME;
-$tbname=TB1NAME;
-
-$sql_conn=mysqli_connect($dbhost, $dbuname, $dbpass);
-$sql_mkdb=mysqli_select_db($sql_conn, $dbname);
-if (!$sql_conn) {
-    die("Connection failed : " . mysqli_connect_error());
-}
-if (!$sql_mkdb) {
-    die("Unable to select db : " . mysqli_error($sql_conn));
-}
+include('db-conn.php');
 
 $uid=$_POST['uname'];
 $pwd=$_POST['pwd'];
-echo $uid,$pwd;
 
-$sql_qr1="SELECT uid, pin_hash FROM $tbname;";
-$sql_arr1=mysqli_query($sql_conn, $sql_qr1);
-while ($u_ph_array=mysqli_fetch_array($sql_arr1)) {
-  $db_ft_pwd=$u_php_array['uid'];
-  if(password_verify($pwd, $db_ft_pwd)) {
-    echo "Password is valid";
-  } else {
-    echo "Password is invalid";
-  }
-}
+$sql_arr1=mysqli_query($sql_conn, "SELECT pin_hash FROM auth WHERE uid='$uid'");
+$phash=mysqli_fetch_assoc($sql_arr1);
+
+if(!$phash)
+  echo "INVALID RETURNED";
+if(password_verify($pwd, $phash['pin_hash']))
+  echo "VALID";
+else
+  echo "ERROR";
+
+/*
+if($sql_arr1=mysqli_query($sql_conn, "SELECT pin_hash FROM auth WHERE uid='$uid'"))
+{
+  $phash=mysqli_fetch_assoc($sql_arr1);
+  if(password_verify($pwd, $phash['pin_hash']))
+    echo "VALID";
+  else
+    echo "ERROR";
+}*/
 
  ?>
